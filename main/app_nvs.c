6e9297b7fb16bbfe1f65012c10fa3318b16ada3f
lucasariasss <ariaslucas1998@gmail.com>
2025-08-02 15:45:54 -0300

    SA-34: Hacer que app_nvs sea compatible con un arreglo de 5 credenciales

--- main/app_nvs.c
+++ main/app_nvs.c
@@ -13,10 +13,19 @@
 #include "nvs_flash.h"
 
 #include "app_nvs.h"
-#include "wifi_app.h"
 
 // Tag for logging the monitor
 static const char TAG[] = "nvs";
+
+typedef struct {
+    char ssid[MAX_SSID_LENGTH];
+    char password[MAX_PASSWORD_LENGTH];
+    uint16_t use_count;   // número de veces que se usó esta credencial
+} wifi_cred_t;
+
+#define MAX_SAVED_CREDS 5
+
+wifi_cred_t saved_creds[MAX_SAVED_CREDS];
 
 // NVS name space used for station mode credentials
 const char app_nvs_sta_creds_namespace[] = "stacreds";
@@ -25,36 +34,58 @@ esp_err_t app_nvs_save_sta_creds(void)
 {
 	nvs_handle handle;
 	esp_err_t esp_err;
-	ESP_LOGI(TAG, "app_nvs_save_sta_creds: Saving station mode credentials to flash");
+	ESP_LOGI(TAG, "app_nvs_save_sta_creds: Salvando credenciales de modo estación en NVS");
 
 	wifi_config_t *wifi_sta_config = wifi_app_get_wifi_config();
+	wifi_cred_t new_cred;
+
+	uint8_t index = 99; // Valor por defecto para indicar que no se encontró un espacio vacío
+
+	strcpy(new_cred.ssid, (const char *)wifi_sta_config->sta.ssid);
+	strcpy(new_cred.password, (const char *)wifi_sta_config->sta.password);
+
+	for(uint8_t i = 0; i < MAX_SAVED_CREDS; i++)
+	{
+		if (saved_creds[i].ssid[0] == '\0') // Encontrar primer espacio vacío
+		{
+			index = i;
+			break;
+		}
+	}
+
+	if (index == 99) // Si no se encontró un espacio vacío, reemplazar la menos usada
+	{
+		uint16_t min_use_count = 65535; // Valor máximo para uint16_t
+		for(uint8_t i = 0; i < MAX_SAVED_CREDS; i++)
+		{
+			if (saved_creds[i].use_count < min_use_count)
+			{
+				min_use_count = saved_creds[i].use_count;
+				index = i;
+			}
+		}
+	}
+
+
 
 	if (wifi_sta_config)
 	{
 		esp_err = nvs_open(app_nvs_sta_creds_namespace, NVS_READWRITE, &handle);
 		if (esp_err != ESP_OK)
 		{
-			printf("app_nvs_save_sta_creds: Error (%s) opening NVS handle\n", esp_err_to_name(esp_err));
+			printf("app_nvs_save_sta_creds: Error (%s) abriendo NVS handle\n", esp_err_to_name(esp_err));
 			return esp_err;
 		}
 
-		// Set SSID
-		esp_err = nvs_set_blob(handle, "ssid", wifi_sta_config->sta.ssid, MAX_SSID_LENGTH);
+		// Establecer credenciales
+		esp_err = nvs_set_blob(handle, "saved_creds", saved_creds, MAX_SSID_LENGTH);
 		if (esp_err != ESP_OK)
 		{
-			printf("app_nvs_save_sta_creds: Error (%s) setting SSID to NVS\n", esp_err_to_name(esp_err));
+			printf("app_nvs_save_sta_creds: Error (%s) estableciendo credenciales a NVS\n", esp_err_to_name(esp_err));
 			return esp_err;
 		}
 
-		// Set Password
-		esp_err = nvs_set_blob(handle, "password", wifi_sta_config->sta.password, MAX_PASSWORD_LENGTH);
-		if (esp_err != ESP_OK)
-		{
-			printf("app_nvs_save_sta_creds: Error (%s) setting password to NVS\n", esp_err_to_name(esp_err));
-			return esp_err;
-		}
-
-		// Commit credentials to NVS
+		// Confirmar credenciales en NVS
 		esp_err = nvs_commit(handle);
 		if (esp_err != ESP_OK)
 		{
@@ -74,55 +105,35 @@ bool app_nvs_load_sta_creds(void)
 	nvs_handle handle;
 	esp_err_t esp_err;
 
-	ESP_LOGI(TAG, "app_nvs_load_sta_creds: Loading Wifi credentials from flash");
+	ESP_LOGI(TAG, "app_nvs_load_sta_creds: Cargando credenciales de modo estación desde NVS");
 
-	if (nvs_open(app_nvs_sta_creds_namespace, NVS_READONLY, &handle) == ESP_OK)
+	esp_err = nvs_open(app_nvs_sta_creds_namespace, NVS_READONLY, &handle);
+	if (esp_err != ESP_OK)
 	{
-		wifi_config_t *wifi_sta_config = wifi_app_get_wifi_config();
-
-		if (wifi_sta_config == NULL)
-		{
-			wifi_sta_config = (wifi_config_t*)malloc(sizeof(wifi_config_t));
-		}
-		memset(wifi_sta_config, 0x00, sizeof(wifi_config_t));
-
-		// Allocate buffer
-		size_t wifi_config_size = sizeof(wifi_config_t);
-		uint8_t *wifi_config_buff = (uint8_t*)malloc(sizeof(uint8_t) * wifi_config_size);
-		memset(wifi_config_buff, 0x00, sizeof(wifi_config_size));
-
-		// Load SSID
-		wifi_config_size = sizeof(wifi_sta_config->sta.ssid);
-		esp_err = nvs_get_blob(handle, "ssid", wifi_config_buff, &wifi_config_size);
-		if (esp_err != ESP_OK)
-		{
-			free(wifi_config_buff);
-			printf("app_nvs_load_sta_creds: (%s) no station SSID found in NVS\n", esp_err_to_name(esp_err));
-			return false;
-		}
-		memcpy(wifi_sta_config->sta.ssid, wifi_config_buff, wifi_config_size);
-
-		// Loadpassword
-		wifi_config_size = sizeof(wifi_sta_config->sta.password);
-		esp_err = nvs_get_blob(handle, "password", wifi_config_buff, &wifi_config_size);
-		if (esp_err != ESP_OK)
-		{
-			free(wifi_config_buff);
-			printf("app_nvs_load_sta_creds: (%s) retrieving password\n", esp_err_to_name(esp_err));
-			return false;
-		}
-		memcpy(wifi_sta_config->sta.password, wifi_config_buff, wifi_config_size);
-
-		free(wifi_config_buff);
-		nvs_close(handle);
-
-		printf("app_nvs_load_sta_creds: SSID: %s Password: %s\n", wifi_sta_config->sta.ssid, wifi_sta_config->sta.password);
-		return wifi_sta_config->sta.ssid[0] != '\0';
-	}
-	else
-	{
+		printf("app_nvs_load_sta_creds: Error (%s) abriendo NVS handle!\n", esp_err_to_name(esp_err));
 		return false;
 	}
+
+	size_t size = sizeof(saved_creds);
+	esp_err = nvs_get_blob(handle, "saved_creds", saved_creds, &size);
+	nvs_close(handle);
+
+	if (esp_err != ESP_OK)
+	{
+		ESP_LOGE(TAG, "No se pudieron leer credenciales: %s", esp_err_to_name(esp_err));
+		return false;
+	}
+
+	wifi_config_t *wifi_sta_config = wifi_app_get_wifi_config();
+	if (wifi_sta_config == NULL)
+	{
+		wifi_sta_config = (wifi_config_t*)malloc(sizeof(wifi_config_t));
+	}
+	memset(wifi_sta_config, 0x00, sizeof(wifi_config_t));
+
+	// Buscar primer credencial válida
+	app_nvs_print_sta_creds();
+	return false;
 }
 
 esp_err_t app_nvs_clear_sta_creds(void)
@@ -159,8 +170,57 @@ esp_err_t app_nvs_clear_sta_creds(void)
 	return ESP_OK;
 }
 
+void app_nvs_print_sta_creds(void)
+{
+	nvs_handle handle;
+	esp_err_t esp_err;
 
+	ESP_LOGI(TAG, "app_nvs_print_sta_creds: Printing saved Wifi station mode credentials");
 
+	if (nvs_open(app_nvs_sta_creds_namespace, NVS_READONLY, &handle) == ESP_OK)
+	{
+		size_t size = sizeof(saved_creds);
+		esp_err = nvs_get_blob(handle, "saved_creds", saved_creds, &size);
+		if (esp_err != ESP_OK)
+		{
+			printf("app_nvs_print_sta_creds: Error (%s) retrieving saved credentials\n", esp_err_to_name(esp_err));
+			nvs_close(handle);
+			return;
+		}
+
+		for (uint8_t i = 0; i < MAX_SAVED_CREDS; i++)
+		{
+			printf("SSID: %s, Password: %s, Use Count: %d\n", saved_creds[i].ssid, saved_creds[i].password, saved_creds[i].use_count);
+		}
+		nvs_close(handle);
+	}
+	else
+	{
+		printf("app_nvs_print_sta_creds: No saved credentials found\n");
+	}
+}	
+
+void app_nvs_struct_array_to_json( cJSON *json)
+{
+    for (int i = 0; i < MAX_SAVED_CREDS; i++) {
+        if (saved_creds[i].ssid[0] != '\0') {     // Verifica si la credencial es válida
+            cJSON *cred_json = cJSON_CreateObject();
+            if (cred_json == NULL) {
+                ESP_LOGE(TAG, "No se pudo crear JSON para credencial %d", i);
+                continue;
+            }
+
+            cJSON_AddStringToObject(cred_json, "ssid", saved_creds[i].ssid);
+            cJSON_AddStringToObject(cred_json, "password", saved_creds[i].password);
+            cJSON_AddNumberToObject(cred_json, "use_count", saved_creds[i].use_count);
+
+            // Se agrega cada objeto creado al arreglo JSON
+            cJSON_AddItemToArray(json, cred_json);
+        } else {
+            ESP_LOGE(TAG, "No hay credencial válida en espacio %d", i);
+        }
+    }
+}
 
 
 
